d011380e9e70f3468954e7db4c9ec4daaff62072
Peter Doan <doanpiotr@gmail.com>
2015-01-01 19:29:45 +0100

    Handle window close button press.

--- source/main.js
+++ source/main.js
@@ -1,6 +1,12 @@
+var gui = require('nw.gui');
+var window = gui.Window.get();
+
 var main = function()
 {
-    
+    $('#window-close').click(function()
+    {
+        window.close();
+    });
 };
 
 $(document).ready(main);
