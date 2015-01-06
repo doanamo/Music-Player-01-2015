9036dcceb6c2121f3e46228b4e893c4f3cca7f74
Peter Doan <doanpiotr@gmail.com>
2015-01-06 19:42:29 +0100

    Move playback panel code to separate file.

--- source/main.js
+++ source/main.js
@@ -4,12 +4,13 @@ gui = new function()
 {
     var self = this;
     
-    this.draggingPlaybackBar = false;
-    
     this.initialize = function()
     {
         self.volumePanel = require("./volume-panel.js");
+        self.playbackPanel = require("./playback-panel.js");
+        
         self.volumePanel.initialize();
+        self.playbackPanel.initialize();
     };
 };
 
@@ -44,45 +45,25 @@ audio = new function()
         var name = file.name.replace(/\.[^/.]+$/, "");
         $('#playback-name').text(name);
         
-        // Set sound track duration when it becomes available.
+        // Set playback event handlers.
         this.sound.bind("durationchange", function()
         {
-            var duration = this.getDuration();
-            $('#playback-time .duration').text(buzz.toTimer(duration));
+            gui.playbackPanel.onDurationChange(this);
         });
         
-        // Set playback event handlers.
         this.sound.bind("playing", function()
         {
-            // Change progress bar style.
-            var bar = $('#playback-progress .progress-bar');
-            bar.addClass('progress-bar-striped active');
-            bar.removeClass('progress-bar-default');
+            gui.playbackPanel.onPlay();
         });
         
         this.sound.bind("pause", function()
         {
-            // Change progress bar style.
-            var bar = $('#playback-progress .progress-bar');
-            bar.removeClass('progress-bar-striped active');
-            bar.addClass('progress-bar-default');
+            gui.playbackPanel.onPause();
         });
         
         this.sound.bind("timeupdate", function()
         {
-            // Don't update if dragging the playback bar.
-            if(gui.draggingPlaybackBar)
-                return;
-            
-            // Get sound track time and duration.
-            var time = this.getTime();
-            var duration = this.getDuration();
-            
-            // Update time text.
-            $('#playback-time .current').text(buzz.toTimer(time));
-            
-            // Update progress bar.
-            $('#playback-progress .progress-bar').css('width', time / duration * 100 + '%');
+            gui.playbackPanel.onTimeUpdate(this);
         });
     };
     
@@ -187,22 +168,8 @@ module.exports = function()
     // Page events.
     $(window.document).mousemove(function(event)
     {
-        if(gui.draggingPlaybackBar)
-        {
-            // Calculate mouse position on the progress bar.
-            var x = event.pageX - $('#playback-progress').offset().left;
-            var alpha = x / $('#playback-progress').width();
-            alpha = Math.clamp(alpha, 0.0, 1.0);
-            
-            // Reflect time of dragged progress bar.
-            var duration = audio.getDuration();
-            $('#playback-time .current').text(buzz.toTimer(duration * alpha));
-            
-            // Update progress bar.
-            $('#playback-progress .progress-bar').css('width', alpha * 100 + '%');
-        }
-        
         gui.volumePanel.onMouseMove(event);
+        gui.playbackPanel.onMouseMove(event);
     });
     
     $(window.document).mousedown(function(event)
@@ -212,22 +179,8 @@ module.exports = function()
     
     $(window.document).mouseup(function(event)
     {
-        if(gui.draggingPlaybackBar)
-        {
-            // Set interface state.
-            gui.draggingPlaybackBar = false;
-            
-            // Restore transition effect.
-            $('#playback-progress .progress-bar').removeClass('no-transition');
-            
-            // Reset if there's no sound track.
-            if(!audio.sound)
-            {
-                $('#playback-progress .progress-bar').css('width', '0%');
-            }
-        }
-
         gui.volumePanel.onMouseUp(event);
+        gui.playbackPanel.onMouseUp(event);
     });
 
     // Application control.
@@ -263,31 +216,6 @@ module.exports = function()
     
     $('#playback-play').click(function()
     {
-        audio.play();
-    });
-    
-    // Progress bar.
-    $('#playback-progress').mousedown(function(event)
-    {
-        // Set interface state.
-        gui.draggingPlaybackBar = true;
-        
-        // Disable transition effect.
-        $('#playback-progress .progress-bar').addClass('no-transition');
-
-        // Prevent selection.
-        return false;
-    });
-    
-    $('#playback-progress').click(function(event)
-    {
-        // Calculate mouse position on the progress bar.
-        var x = event.pageX - $(this).offset().left;
-        var alpha = x / $(this).width();
-        alpha = Math.clamp(alpha, 0.0, 1.0);
-
-        // Change playback position.
-        audio.setPercent(alpha * 100);
         audio.play();
     });
 };
---
+++ source/playback-panel.js
@@ -0,0 +1,109 @@
+module.exports = new function()
+{
+    var self = this;
+    var draggingProgressBar = false;
+    
+    this.initialize = function()
+    {
+        $('#playback-progress').mousedown(function(event)
+        {
+            // Set progress bar state.
+            self.draggingProgressBar = true;
+            
+            // Disable transition effect.
+            $('#playback-progress .progress-bar').addClass('no-transition');
+
+            // Prevent selection.
+            return false;
+        });
+        
+        $('#playback-progress').click(function(event)
+        {
+            // Calculate mouse position on the progress bar.
+            var x = event.pageX - $(this).offset().left;
+            var alpha = x / $(this).width();
+            alpha = Math.clamp(alpha, 0.0, 1.0);
+
+            // Change playback position.
+            audio.setPercent(alpha * 100);
+            audio.play();
+        });
+    };
+    
+    this.onDurationChange = function(sound)
+    {
+        // Some metadata might not be available immediately after opening a sound track.
+        // Thats why this event callback is needed to handle this info later on.
+        var duration = sound.getDuration();
+        $('#playback-time .duration').text(buzz.toTimer(duration));
+    };
+    
+    this.onPlay = function()
+    {
+        // Change progress bar style.
+        var bar = $('#playback-progress .progress-bar');
+        bar.addClass('progress-bar-striped active');
+        bar.removeClass('progress-bar-default');
+    };
+    
+    this.onPause = function()
+    {
+        // Change progress bar style.
+        var bar = $('#playback-progress .progress-bar');
+        bar.removeClass('progress-bar-striped active');
+        bar.addClass('progress-bar-default');
+    };
+    
+    this.onTimeUpdate = function(sound)
+    {
+        // Don't update if dragging the progress bar.
+        if(self.draggingProgressBar)
+            return;
+        
+        // Get sound track time and duration.
+        var time = sound.getTime();
+        var duration = sound.getDuration();
+        
+        // Update time text.
+        $('#playback-time .current').text(buzz.toTimer(time));
+        
+        // Update progress bar.
+        $('#playback-progress .progress-bar').css('width', time / duration * 100 + '%');
+    };
+    
+    this.onMouseMove = function(event)
+    {
+        if(self.draggingProgressBar)
+        {
+            // Calculate mouse position on the progress bar.
+            var x = event.pageX - $('#playback-progress').offset().left;
+            var alpha = x / $('#playback-progress').width();
+            alpha = Math.clamp(alpha, 0.0, 1.0);
+            
+            // Reflect time of progress bar position.
+            var duration = audio.getDuration();
+            $('#playback-time .current').text(buzz.toTimer(duration * alpha));
+            
+            // Update progress bar.
+            $('#playback-progress .progress-bar').css('width', alpha * 100 + '%');
+        }
+    };
+    
+    this.onMouseUp = function(event)
+    {
+        if(self.draggingProgressBar)
+        {
+            // Set progress bar state.
+            self.draggingProgressBar = false;
+            
+            // Restore transition effect.
+            $('#playback-progress .progress-bar').removeClass('no-transition');
+            
+            // Reset if there's no sound track.
+            if(!audio.sound)
+            {
+                $('#playback-progress .progress-bar').css('width', '0%');
+            }
+        }
+    };
+};
