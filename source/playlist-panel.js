0fc7302be9d5b6d0c580114d600db16bdbe3d4b4
Piotr Doan <doanpiotr@gmail.com>
2015-01-11 13:49:58 +0100

    Implement basic playlist creation.

--- source/playlist-panel.js
+++ source/playlist-panel.js
@@ -6,11 +6,32 @@ module.exports = new function()
     {
         for(var i = 0; i < 4; ++i)
         {
-            var element = $('<li>');
-            element.addClass('list-group-item');
-            element.append($('<div>').text("Playlist " + i));
-            
-            $('#playlist-panel .list-group').append(element);
+            self.createList("Playlist " + i);
         }
+    };
+    
+    this.createList = function(name)
+    {
+        // Create list element.
+        var element = $('<li>');
+        element.addClass('list-group-item');
+        element.append($('<div>').text(name));
+        
+        element.click(function(event)
+        {
+            self.setCurrentList(this);
+        });
+        
+        // Add element to the list.
+        $('#playlist-panel .list-group').append(element);
+        
+        // Prevent cyclic reference.
+        element = null;
+    };
+    
+    this.setCurrentList = function(element)
+    {
+        $(element).siblings().removeClass('active');
+        $(element).addClass('active');
     };
 };
